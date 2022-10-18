69460f0b0da929b550071e886b9acb1d064e93fa
“JusticeDibie” <justicedibie@yahoo.com>
2022-10-18 12:20:33 -0700

    nested

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+**main - Entry point
+*
+*@char : description, its a character type
+*@_putchar() : is a funcyion
+*Return: Always 0 (success)
+*
+*/
+
+
+char _putchar(char);
+int main(void)
+{
+char *ch = "_putchar";
+while (*ch)
+{
+_putchar(*ch);
+ch++;
+}
+{
+_putchar('\n');
+}
+return (0);
+}
